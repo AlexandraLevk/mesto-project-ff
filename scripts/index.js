655cf94f01cb0ce78c4aa14d1e1df7c7bd2a0a5d
Alexandra Levkoeva <sasha.levkoeva@ya.ru>
2023-11-21 13:59:44 +0300

    JS added

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
-// @todo: Темплейт карточки
-
 // @todo: DOM узлы
+const content = document.querySelector('.content');
+const cardsList = content.querySelector('.places__list');
+
+// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: Функция создания карточки
+const createCard = (cardLink, cardName, deleteCardCallback) => {
+  const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = cardItem.querySelector('.card__delete-button');
+  cardItem.querySelector('.card__image').src = cardLink;
+  cardItem.querySelector('.card__title').textContent = cardName;
+  deleteButton.addEventListener('click', deleteCardCallback);
+  return cardItem;
+};
+
+// Функция вставки карточки в разметку
+const addCard = (item) => {
+  const cardItem = createCard(item.link, item.name, deleteCardCallback);
+  cardsList.append(cardItem);
+};
 
 // @todo: Функция удаления карточки
+const deleteCardCallback = (evt) => {
+  evt.target.closest('.card').remove();
+};
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item) => {
+  addCard(item, deleteCardCallback);
+});
